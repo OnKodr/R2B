6dace86eacc950661a9bf5d915d30b2d466fa9be
OnKodr <36519431+OnKodr@users.noreply.github.com>
2018-04-02 22:37:13 +0200

    Adding idnes

---
+++ idnes/index.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>R2B</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+    </head>
+    <body>
+        <div class="banner">
+            <a href="https://www.mall.cz">
+                <img id="logoMall" src="https://d48-a.sdn.szn.cz/d_48/c_img_G_C/r3GB0VL.png" alt="Mall.cz">
+            </a>
+            <div class="one">
+                <a class="title" href="https://www.mall.cz/notebooky/acer-aspire-es11-nxgglec004?src=sug">
+                    Acer Aspire ES11 (NX.GGLEC.004)
+                </a>
+                <div class="infoOne clearfix">
+                    <div class="leftFirst">
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Poměr cena/výkon</p>
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Přenosný</p>
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Výdrž baterie</p>
+                    </div>
+                    <div class="rightFirst">
+                        <a href="https://www.mall.cz/notebooky/acer-aspire-es11-nxgglec004?src=sug">
+                        <img src="https://d25-a.sdn.szn.cz/d_25/c_img_G_CK/YYRCCCT.jpeg" alt="Acer Aspire ES11">
+                        </a>
+                    </div>
+                </div>
+                <div class="price clearfix">
+                    <p>naše cena: </p>
+                    <span>5 490,-</span>
+                </div>
+            </div>
+            <div class="two">
+                <div class="infoTwo clearfix">
+                    <a class="title" href="https://www.mall.cz/notebooky/acer-spin-1-nxgmbec002?ppcbee-adtext-variant=2018-03-Spl%C3%A1tky-bez-nav%C3%BD%C5%A1en%C3%AD-nad-1000-K%C4%8D&gclid=Cj0KCQjw-uzVBRDkARIsALkZAdlRz6512DiqG_MFIsVxDElpatdkFk5NZcUGZVOCOqWETnbJPSOkirIaAgXHEALw_wcB">
+                        Acer Spin 1 (NX.GMBEC.002)
+                    </a>
+                    <div class="leftSecond">
+                        <a href="https://www.mall.cz/notebooky/acer-spin-1-nxgmbec002?ppcbee-adtext-variant=2018-03-Spl%C3%A1tky-bez-nav%C3%BD%C5%A1en%C3%AD-nad-1000-K%C4%8D&gclid=Cj0KCQjw-uzVBRDkARIsALkZAdncdTL1dOAZ7eFT0BrdbUBHu83EPXeaRsMN3ksFpII3OZvw6OULIP8aAoQhEALw_wcB">
+                        <img src="https://d25-a.sdn.szn.cz/d_25/c_img_F_Ch/Tify4M.jpeg" alt="Acer Aspire ES11">
+                        </a>
+                    </div>
+                    <div class="rightSecond">
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Poměr cena/výkon</p>
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Kvalitní zpracování</p>
+                        <p><i class="fa fa-plus" aria-hidden="true"></i>Přenosný</p>
+                    </div>
+                </div>
+                <div class="price clearfix">
+                    <p>naše cena: </p>
+                    <span>8 490,-</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
